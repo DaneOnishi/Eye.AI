91d9877310c4f31ee214d5ccac83f49f2ae4581b
Bruno Pastre <pastre68@gmail.com>
2021-06-13 00:34:07 -0300

    File organization

--- ios/SpatialFarmers/SpatialFarmers/ViewController.swift
+++ ios/SpatialFarmers/SpatialFarmers/ViewController.swift
@@ -11,24 +11,82 @@ import Vision
 
 final class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, AVSpeechSynthesizerDelegate {
     
+    // MARK: - UI Elements
+    
     @IBOutlet weak var debugImage: UIImageView!
     @IBOutlet weak var contentLabe: UILabel!
     @IBOutlet weak var confiidenceLabel: UILabel!
     
+    // MARK: - Properties
+    
+    private let speechSynthesizer = AVSpeechSynthesizer()
     private let context = CIContext(options: nil)
     private var session: AVCaptureSession!
     private var device: AVCaptureDevice!
     
     private var isTalking: Bool = false
     
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCaptureSession()
+    }
+    
+    // MARK: - Delegates
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        isTalking.toggle()
+    }
+    
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         if isTalking { return }
         handleNewFrame(using: sampleBuffer)
     }
     
+    // MARK: - Provate methods
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private func handleRecognitionResult(request: VNRequest, error: Error?) {
+           guard let results = request.results,
+                 let observations = results as? [VNRecognizedTextObservation]
+           else { return }
+           let observedResults = observations.compactMap { $0.topCandidates(1).first }
+           
+           let averageConfidence = observedResults.map { Float($0.confidence) }.reduce(.zero, +) / Float(observedResults.count)
+           let readText = observedResults.map { $0.string }.joined(separator: " ")
+           
+//            guard averageConfidence > 0.8 else { return }
+           DispatchQueue.main.async {
+               
+               self.confiidenceLabel.text = "\(averageConfidence)"
+               self.contentLabe.text = readText
+               let speechUtterance = AVSpeechUtterance(string: readText)
+               self.speechSynthesizer.delegate = self
+               speechUtterance.voice = AVSpeechSynthesisVoice(language: "pt-BR")
+               self.isTalking = true
+               self.speechSynthesizer.speak(speechUtterance)
+           }
+    }
+    
+    private func handleNewFrame(using buffer: CMSampleBuffer) {
+        let imageBuffer = CMSampleBufferGetImageBuffer(buffer)!
+        let ciimage = CIImage(cvPixelBuffer: imageBuffer).oriented(.right)
+        let cgImage = context.createCGImage(ciimage, from: ciimage.extent)!
+        
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        let request = VNRecognizeTextRequest(completionHandler: handleRecognitionResult)
+        
+        request.usesLanguageCorrection = true
+        request.recognitionLevel = .accurate
+        
+        do {
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
+    }
+    
+    private func configureCaptureSession() {
         session = .init()
         session.beginConfiguration()
         session.sessionPreset = .high
@@ -52,56 +110,6 @@ final class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         (view as? PreviewView)?.sessionLayer?.session = session
         
         session.startRunning()
-    }
-    
-    
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
-        isTalking.toggle()
-    }
-    let speechSynthesizer = AVSpeechSynthesizer()
-    private func handleNewFrame(using buffer: CMSampleBuffer) {
-        let imageBuffer = CMSampleBufferGetImageBuffer(buffer)!
-        let ciimage = CIImage(cvPixelBuffer: imageBuffer).oriented(.right)
-        let cgImage = context.createCGImage(ciimage, from: ciimage.extent)!
-        let debugImage = UIImage(cgImage: cgImage)
-        
-        
-        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
-        let request = VNRecognizeTextRequest { (request, error) in
-            print("request")
-            guard let results = request.results,
-                  let observations = results as? [VNRecognizedTextObservation]
-            else { return }
-            let observedResults = observations.compactMap { $0.topCandidates(1).first }
-            
-            let averageConfidence = observedResults.map { Float($0.confidence) }.reduce(.zero, +) / Float(observedResults.count)
-            let readText = observedResults.map { $0.string }.joined(separator: " ")
-            
-//            guard averageConfidence > 0.8 else { return }
-            DispatchQueue.main.async {
-                
-                self.confiidenceLabel.text = "\(averageConfidence)"
-                self.contentLabe.text = readText
-                let speechUtterance = AVSpeechUtterance(string: readText)
-                self.speechSynthesizer.delegate = self
-                speechUtterance.voice = AVSpeechSynthesisVoice(language: "pt-BR")
-                self.isTalking = true
-                self.speechSynthesizer.speak(speechUtterance)
-                
-                
-                self.debugImage.image = debugImage
-            }
-            
-        }
-        
-        request.usesLanguageCorrection = true
-        request.recognitionLevel = .accurate
-        
-        do {
-            try requestHandler.perform([request])
-        } catch {
-            print("Unable to perform the requests: \(error).")
-        }
     }
 }
 
